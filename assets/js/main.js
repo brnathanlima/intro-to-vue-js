62bafb4fac4e770ba26b792f130752340cb25547
Nathanael Lima <nathanael.lima@orubetecnologia.com.br>
2020-05-04 12:11:08 -0300

    Adding flag due to inventory status and button for buy

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ var app = new Vue({
         description: "Lorem ipsum",
         image: "./assets/images/vmSocks-green-onWhite.jpg",
         alt: "Picture of a pair of green socks with Vue Mastery logo at the ancle.",
-        link: "#"
+        link: "#",
+        inventory: 0
     }
 });
