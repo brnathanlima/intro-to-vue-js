b54189d9928a9bac26f2f237eb9548358800a08f
Nathanael Lima <nathanael.lima@orubetecnologia.com.br>
2020-05-04 18:42:49 -0300

    Add and remove to cart functionalities

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,44 @@ var app = new Vue({
     data: {
         product: "Socks",
         description: "Lorem ipsum",
-        image: "./assets/images/vmSocks-green-onWhite.jpg",
-        alt: "Picture of a pair of green socks with Vue Mastery logo at the ancle.",
         link: "#",
         inventory: 10,
+        image: "./assets/images/vmSocks-green-onWhite.jpg",
+        alt: "Picture of a pair of green socks with Vue Mastery logo at the ancle.",
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 1,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: "./assets/images/vmSocks-green-onWhite.jpg",
+                variantAlt: "Picture of a pair of green socks with Vue Mastery logo at the ancle.",
             },
             {
                 variantId: 2,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: "./assets/images/vmSocks-blue-onWhite.jpg",
+                variantAlt: "Picture of a pair of blue socks with Vue Mastery logo at the ancle.",
             }
         ],
-        sizes: ["29-34", "35-39", "40-45"]
+        sizes: ["29-34", "35-39", "40-45"],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            ++this.cart;
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                --this.cart;                
+            }
+        },
+        changeProductImage(variantId) {
+            this.variants.forEach(variant => {
+                if (variant.variantId == variantId) {                    
+                    this.image = variant.variantImage;
+                    this.alt = variant.variantAlt;
+                }
+            });
+        }
     }
 });
