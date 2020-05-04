4aebab0e14e36c3f497c756c358928a294441be4
Nathanael Lima <nathanael.lima@orubetecnologia.com.br>
2020-05-04 11:24:52 -0300

    Adding style and image to produt page

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "Lorem ipsum",
+        image: "./assets/images/vmSocks-green-onWhite.jpg",
+        alt: "Picture of a pair of green socks with Vue Mastery logo at the ancle.",
+        link: "#"
+    }
+});
--- main.js
+++
@@ -1,7 +0,0 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        description: "Lorem ipsum"
-    }
-});
